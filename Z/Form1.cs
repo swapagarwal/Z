7e229d507c6c56d6dfa38e2c37e48b851eecbd37
Swapnil Agarwal <a.swapnil@iitg.ernet.in>
2016-03-11 00:50:29 +0530

    Add volume control

---
+++ Z/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Z
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SetVolume(int level)
+        {
+            try
+            {
+                //Instantiate an Enumerator to find audio devices
+                NAudio.CoreAudioApi.MMDeviceEnumerator MMDE = new NAudio.CoreAudioApi.MMDeviceEnumerator();
+                //Get all the devices, no matter what condition or status
+                NAudio.CoreAudioApi.MMDeviceCollection DevCol = MMDE.EnumerateAudioEndPoints(NAudio.CoreAudioApi.DataFlow.All, NAudio.CoreAudioApi.DeviceState.All);
+                //Loop through all devices
+                foreach (NAudio.CoreAudioApi.MMDevice dev in DevCol)
+                {
+                    try
+                    {
+                        if (dev.State == NAudio.CoreAudioApi.DeviceState.Active)
+                        {
+                            var newVolume = (float)Math.Max(Math.Min(level, 100), 0) / (float)100;
+
+                            //Set at maximum volume
+                            dev.AudioEndpointVolume.MasterVolumeLevelScalar = newVolume;
+
+                            dev.AudioEndpointVolume.Mute = level == 0;
+
+                            //Get its audio volume
+                            //_log.Info("Volume of " + dev.FriendlyName + " is " + dev.AudioEndpointVolume.MasterVolumeLevelScalar.ToString());
+                        }
+                        else
+                        {
+                            //_log.Debug("Ignoring device " + dev.FriendlyName + " with state " + dev.State);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        //Do something with exception when an audio endpoint could not be muted
+                        //_log.Warn(dev.FriendlyName + " could not be muted with error " + ex);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //When something happend that prevent us to iterate through the devices
+                //_log.Warn("Could not enumerate devices due to an excepion: " + ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int vol = Int32.Parse(textBox1.Text);
+                SetVolume(vol);
+            }
+            catch
+            {
+                SetVolume(50);
+            }
+        }
+    }
+}
