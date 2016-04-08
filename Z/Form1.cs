1c7b7a8a0e3c1d3bafa069cc1bcbd335b05afacd
Swapnil Agarwal <swapnilagarwal@live.com>
2016-04-08 11:17:46 +0530

    brightness working \m/

--- Z/Form1.cs
+++ Z/Form1.cs
@@ -13,9 +13,13 @@ namespace Z
 {
     public partial class Form1 : Form
     {
+        byte[] bLevels; //array of valid level values
+
         public Form1()
         {
             InitializeComponent();
+            bLevels = GetBrightnessLevels();
+            textBox2.Text = GetBrightness().ToString();
         }
 
         private void SetVolume(int level)
@@ -75,40 +79,125 @@ namespace Z
             {
                 SetVolume(50);
             }
-            Brightness();
-            //SetBrightness(100);
         }
-        public void Brightness()
-        {
-            string NamespacePath = "\\\\.\\ROOT\\WMI";
-            string ClassName = "WmiMonitorBrightnessEvent";
 
-            ManagementClass mClass = new ManagementClass(NamespacePath + ":" + ClassName);
-            PropertyDataCollection lproperties = mClass.Properties;
-            Console.WriteLine(string.Format("Property Names in {0}: ", ClassName));
-            foreach (PropertyData property in lproperties)
-            {
-                Console.WriteLine("name: {0}, Origin: {1}", property.Name, property.Value);
-                //string brightness_Value = property.ToString();
-                //Console.WriteLine();
-            }
-        }
-        static void SetBrightness(byte targetBrightness)
+        private void button2_Click(object sender, EventArgs e)
         {
-            ManagementScope scope = new ManagementScope("root\\WMI");
-            SelectQuery query = new SelectQuery("WmiMonitorBrightnessMethods");
-            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query))
+            try
             {
-                using (ManagementObjectCollection objectCollection = searcher.Get())
+                int iPercent = Convert.ToInt16(textBox2.Text);
+                if (iPercent >= 0 && iPercent <= bLevels[bLevels.Count() - 1])
                 {
-                    foreach (ManagementObject mObj in objectCollection)
+                    byte level = 100;
+                    foreach (byte item in bLevels)
                     {
-                        mObj.InvokeMethod("WmiSetBrightness",
-                            new Object[] { UInt32.MaxValue, targetBrightness });
-                        break;
+                        if (item >= iPercent)
+                        {
+                            level = item;
+                            break;
+                        }
                     }
+                    SetBrightness(level);
+                }
+                else
+                {
+                    textBox2.Text = "Something happened!";
                 }
             }
+            catch
+            {
+                SetBrightness(50);
+            }
+        }
+
+        //get the actual percentage of brightness
+        static int GetBrightness()
+        {
+            //define scope (namespace)
+            System.Management.ManagementScope s = new System.Management.ManagementScope("root\\WMI");
+
+            //define query
+            System.Management.SelectQuery q = new System.Management.SelectQuery("WmiMonitorBrightness");
+
+            //output current brightness
+            System.Management.ManagementObjectSearcher mos = new System.Management.ManagementObjectSearcher(s, q);
+
+            System.Management.ManagementObjectCollection moc = mos.Get();
+
+            //store result
+            byte curBrightness = 0;
+            foreach (System.Management.ManagementObject o in moc)
+            {
+                curBrightness = (byte)o.GetPropertyValue("CurrentBrightness");
+                break; //only work on the first object
+            }
+
+            moc.Dispose();
+            mos.Dispose();
+
+            return (int)curBrightness;
+        }
+
+        //array of valid brightness values in percent
+        static byte[] GetBrightnessLevels()
+        {
+            //define scope (namespace)
+            System.Management.ManagementScope s = new System.Management.ManagementScope("root\\WMI");
+
+            //define query
+            System.Management.SelectQuery q = new System.Management.SelectQuery("WmiMonitorBrightness");
+
+            //output current brightness
+            System.Management.ManagementObjectSearcher mos = new System.Management.ManagementObjectSearcher(s, q);
+            byte[] BrightnessLevels = new byte[0];
+
+            try
+            {
+                System.Management.ManagementObjectCollection moc = mos.Get();
+
+                //store result
+
+
+                foreach (System.Management.ManagementObject o in moc)
+                {
+                    BrightnessLevels = (byte[])o.GetPropertyValue("Level");
+                    break; //only work on the first object
+                }
+
+                moc.Dispose();
+                mos.Dispose();
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Sorry, Your System does not support this brightness control...");
+
+            }
+
+            return BrightnessLevels;
+        }
+
+        static void SetBrightness(byte targetBrightness)
+        {
+            //define scope (namespace)
+            System.Management.ManagementScope s = new System.Management.ManagementScope("root\\WMI");
+
+            //define query
+            System.Management.SelectQuery q = new System.Management.SelectQuery("WmiMonitorBrightnessMethods");
+
+            //output current brightness
+            System.Management.ManagementObjectSearcher mos = new System.Management.ManagementObjectSearcher(s, q);
+
+            System.Management.ManagementObjectCollection moc = mos.Get();
+
+            foreach (System.Management.ManagementObject o in moc)
+            {
+                o.InvokeMethod("WmiSetBrightness", new Object[] { UInt32.MaxValue, targetBrightness }); //note the reversed order - won't work otherwise!
+                break; //only work on the first object
+            }
+
+            moc.Dispose();
+            mos.Dispose();
         }
     }
 }
