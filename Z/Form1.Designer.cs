73ebf6d3c2e5f5bde14eec5b9007950474e39a06
Sankalp Bihani <sankalpbihani.008@gmail.com>
2016-04-11 16:06:36 +0530

    cleanup

--- Z/Form1.Designer.cs
+++ Z/Form1.Designer.cs
@@ -28,48 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.volume_mode = new System.Windows.Forms.CheckBox();
             this.brightness_mode = new System.Windows.Forms.CheckBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.application_searcher = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Applications = new System.Windows.Forms.DataGridViewLinkColumn();
-            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(165, 60);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(87, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Set Volume";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(30, 60);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(30, 86);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
             // 
             // volume_mode
             // 
             this.volume_mode.AutoSize = true;
-            this.volume_mode.Location = new System.Drawing.Point(30, 13);
+            this.volume_mode.Location = new System.Drawing.Point(12, 12);
             this.volume_mode.Name = "volume_mode";
             this.volume_mode.Size = new System.Drawing.Size(111, 17);
             this.volume_mode.TabIndex = 3;
@@ -79,38 +49,18 @@
             // brightness_mode
             // 
             this.brightness_mode.AutoSize = true;
-            this.brightness_mode.Location = new System.Drawing.Point(30, 37);
+            this.brightness_mode.Location = new System.Drawing.Point(147, 12);
             this.brightness_mode.Name = "brightness_mode";
             this.brightness_mode.Size = new System.Drawing.Size(125, 17);
             this.brightness_mode.TabIndex = 4;
             this.brightness_mode.Text = "Automatic Brightness";
             this.brightness_mode.UseVisualStyleBackColor = true;
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(165, 89);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(87, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Set Brightness";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(165, 118);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(87, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Check Process";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // application_searcher
             // 
-            this.application_searcher.Location = new System.Drawing.Point(30, 152);
+            this.application_searcher.Location = new System.Drawing.Point(12, 35);
             this.application_searcher.Name = "application_searcher";
-            this.application_searcher.Size = new System.Drawing.Size(242, 20);
+            this.application_searcher.Size = new System.Drawing.Size(260, 20);
             this.application_searcher.TabIndex = 6;
             this.application_searcher.TextChanged += new System.EventHandler(this.application_searcher_TextChanged);
             // 
@@ -122,12 +72,12 @@
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Applications});
             this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-            this.dataGridView1.Location = new System.Drawing.Point(30, 178);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 61);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowTemplate.ReadOnly = true;
             this.dataGridView1.ShowEditingIcon = false;
-            this.dataGridView1.Size = new System.Drawing.Size(242, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(260, 267);
             this.dataGridView1.TabIndex = 8;
             // 
             // Applications
@@ -138,31 +88,15 @@
             this.Applications.Text = "";
             this.Applications.TrackVisitedState = false;
             // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(80, 118);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 9;
-            this.button4.Text = "Usage";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 336);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.application_searcher);
             this.Controls.Add(this.brightness_mode);
             this.Controls.Add(this.volume_mode);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -172,18 +106,11 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.CheckBox volume_mode;
         private System.Windows.Forms.CheckBox brightness_mode;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox application_searcher;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewLinkColumn Applications;
-        private System.Windows.Forms.Button button4;
     }
 }
 
--- Z/Form1.cs
+++ Z/Form1.cs
@@ -16,7 +16,7 @@ namespace Z
         ApplicationModel ApplicationData = new ApplicationModel();
         string LastUsedApplication = "";
         string SecondLastUsedApplication = ""; 
-        byte[] bLevels;
+        int UsageCounter = 0;
 
         public Form1()
         {
@@ -30,7 +30,10 @@ namespace Z
             ProcessTimer.Interval = Interval;
             ProcessTimer.Start();
 
-            bLevels = GetBrightnessLevels();
+            System.Timers.Timer UsageCollectionTimer = new System.Timers.Timer();
+            UsageCollectionTimer.Elapsed += new ElapsedEventHandler(DisplayUsage);
+            UsageCollectionTimer.Interval = 1000;
+            UsageCollectionTimer.Start();
 
             dataGridView1.DefaultCellStyle.SelectionBackColor = dataGridView1.DefaultCellStyle.BackColor;
             dataGridView1.DefaultCellStyle.SelectionForeColor = dataGridView1.DefaultCellStyle.ForeColor;
@@ -61,257 +64,7 @@ namespace Z
             LearningTools.ProcessBrightness(brightness_mode.Checked);
             BasicTools.GetTopWindowName();
         }
-
-        private void SetVolume(int level)
-        {
-            try
-            {
-                //Instantiate an Enumerator to find audio devices
-                NAudio.CoreAudioApi.MMDeviceEnumerator MMDE = new NAudio.CoreAudioApi.MMDeviceEnumerator();
-
-                //Get all the devices, no matter what condition or status
-                NAudio.CoreAudioApi.MMDeviceCollection DevCol = MMDE.EnumerateAudioEndPoints(NAudio.CoreAudioApi.DataFlow.All, NAudio.CoreAudioApi.DeviceState.All);
-
-                //Loop through all devices
-                foreach (NAudio.CoreAudioApi.MMDevice dev in DevCol)
-                {
-                    try
-                    {
-                        if (dev.State == NAudio.CoreAudioApi.DeviceState.Active)
-                        {
-                            var newVolume = (float)Math.Max(Math.Min(level, 100), 0) / (float)100;
-
-                            //Set at maximum volume
-                            dev.AudioEndpointVolume.MasterVolumeLevelScalar = newVolume;
-
-                            dev.AudioEndpointVolume.Mute = level == 0;
-
-                            //Get its audio volume
-                            //_log.Info("Volume of " + dev.FriendlyName + " is " + dev.AudioEndpointVolume.MasterVolumeLevelScalar.ToString());
-                        }
-                        else
-                        {
-                            //_log.Debug("Ignoring device " + dev.FriendlyName + " with state " + dev.State);
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        //Do something with exception when an audio endpoint could not be muted
-                        //_log.Warn(dev.FriendlyName + " could not be muted with error " + ex);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                //When something happend that prevent us to iterate through the devices
-                //_log.Warn("Could not enumerate devices due to an excepion: " + ex.Message);
-            }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                int vol = int.Parse(textBox1.Text);
-                SetVolume(vol);
-            }
-            catch
-            {
-                SetVolume(50);
-            }
-        }
         
-        private void button2_Click(object sender, EventArgs e)
-        {
-            int iPercent;
-            try
-            {
-                iPercent = Convert.ToInt16(textBox2.Text);
-            }
-            catch
-            {
-                iPercent = 50;
-            }
-            if (iPercent >= 0 && iPercent <= bLevels[bLevels.Count() - 1])
-            {
-                byte level = 100;
-                foreach (byte item in bLevels)
-                {
-                    if (item >= iPercent)
-                    {
-                        level = item;
-                        break;
-                    }
-                }
-                SetBrightness(level);
-            }
-            else
-            {
-                textBox2.Text = "Something happened!";
-            }
-        }
-
-        //get the actual percentage of brightness
-        static int GetBrightness()
-        {
-            //define scope (namespace)
-            ManagementScope s = new ManagementScope("root\\WMI");
-
-            //define query
-            SelectQuery q = new SelectQuery("WmiMonitorBrightness");
-
-            //output current brightness
-            ManagementObjectSearcher mos = new ManagementObjectSearcher(s, q);
-
-            ManagementObjectCollection moc = mos.Get();
-
-            //store result
-            byte curBrightness = 0;
-            foreach (ManagementObject o in moc)
-            {
-                curBrightness = (byte)o.GetPropertyValue("CurrentBrightness");
-                break; //only work on the first object
-            }
-
-            moc.Dispose();
-            mos.Dispose();
-
-            return (int)curBrightness;
-        }
-
-        //array of valid brightness values in percent
-        static byte[] GetBrightnessLevels()
-        {
-            //define scope (namespace)
-            ManagementScope s = new ManagementScope("root\\WMI");
-
-            //define query
-            SelectQuery q = new SelectQuery("WmiMonitorBrightness");
-
-            //output current brightness
-            ManagementObjectSearcher mos = new ManagementObjectSearcher(s, q);
-            byte[] BrightnessLevels = new byte[0];
-
-            try
-            {
-                ManagementObjectCollection moc = mos.Get();
-
-                //store result
-
-
-                foreach (ManagementObject o in moc)
-                {
-                    BrightnessLevels = (byte[])o.GetPropertyValue("Level");
-                    break; //only work on the first object
-                }
-
-                moc.Dispose();
-                mos.Dispose();
-
-            }
-            catch (Exception)
-            {
-                Debug.WriteLine("Sorry, Your System does not support this brightness control...");
-            }
-
-            return BrightnessLevels;
-        }
-
-        static void SetBrightness(byte targetBrightness)
-        {
-            //define scope (namespace)
-            ManagementScope s = new ManagementScope("root\\WMI");
-
-            //define query
-            SelectQuery q = new SelectQuery("WmiMonitorBrightnessMethods");
-
-            //output current brightness
-            ManagementObjectSearcher mos = new ManagementObjectSearcher(s, q);
-
-            ManagementObjectCollection moc = mos.Get();
-
-            foreach (ManagementObject o in moc)
-            {
-                o.InvokeMethod("WmiSetBrightness", new Object[] { uint.MaxValue, targetBrightness }); //note the reversed order - won't work otherwise!
-                break; //only work on the first object
-            }
-
-            moc.Dispose();
-            mos.Dispose();
-        }
-
-        #region DLL Imports
-
-        [DllImport("user32.dll")]
-        private static extern IntPtr GetForegroundWindow();
-
-        [DllImport("user32.dll")]
-        static extern int GetWindowTextLength(IntPtr hWnd);
-
-        [DllImport("user32.dll")]
-        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
-
-        [DllImport("user32.dll")]
-        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
-
-        [DllImport("kernel32.dll")]
-        private static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, uint dwProcessId);
-
-        [DllImport("kernel32.dll")]
-        private static extern bool CloseHandle(IntPtr handle);
-
-        [DllImport("psapi.dll")]
-        private static extern uint GetModuleBaseName(IntPtr hWnd, IntPtr hModule, StringBuilder lpFileName, int nSize);
-
-        [DllImport("psapi.dll")]
-        private static extern uint GetModuleFileNameEx(IntPtr hWnd, IntPtr hModule, StringBuilder lpFileName, int nSize);
-
-        #endregion
-
-        public static string GetTopWindowText()
-        {
-            IntPtr hWnd = GetForegroundWindow();
-            int length = GetWindowTextLength(hWnd);
-            StringBuilder text = new StringBuilder(length + 1);
-            GetWindowText(hWnd, text, text.Capacity);
-            return text.ToString();
-        }
-
-        public static string GetTopWindowName()
-        {
-            IntPtr hWnd = GetForegroundWindow();
-            uint lpdwProcessId;
-            GetWindowThreadProcessId(hWnd, out lpdwProcessId);
-
-            IntPtr hProcess = OpenProcess(0x0410, false, lpdwProcessId);
-
-            StringBuilder text = new StringBuilder(1000);
-            StringBuilder text2 = new StringBuilder(1000);
-            GetModuleBaseName(hProcess, IntPtr.Zero, text, text.Capacity);
-            GetModuleFileNameEx(hProcess, IntPtr.Zero, text2, text2.Capacity);
-
-            CloseHandle(hProcess);
-
-            return text.ToString() + " # " + text2.ToString();
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            var x = Process.GetProcesses();
-
-            System.Timers.Timer myTimer = new System.Timers.Timer();
-            myTimer.Elapsed += new ElapsedEventHandler(DisplayTopWindow);
-            myTimer.Interval = 1000; // ms
-            myTimer.Start();
-        }
-
-        public static void DisplayTopWindow(object source, ElapsedEventArgs e)
-        {
-            // will run every second
-            Console.WriteLine(GetTopWindowName());
-            Console.WriteLine(GetTopWindowText());
-        }
-
         private void application_searcher_TextChanged(object sender, EventArgs e)
         {
             dataGridView1.Rows.Clear();
@@ -334,28 +87,17 @@ namespace Z
                 }
             }
         }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            var x = Process.GetProcesses();
-
-            System.Timers.Timer myTimer = new System.Timers.Timer();
-            myTimer.Elapsed += new ElapsedEventHandler(DisplayUsage);
-            myTimer.Interval = 1000; // ms
-            myTimer.Start();
-        }
-
-        static int counter;
+        
         public void DisplayUsage(object sender, EventArgs e)
         {
             Usage.CurrentApplication();
-            counter++;
-            if (counter == 20)
+            UsageCounter++;
+            if (UsageCounter == 20)
             {
                 Usage.displayResult();
-                counter = 0;
+                UsageCounter = 0;
             }
-            else if (counter == 10)
+            else if (UsageCounter == 10)
             {
                 Usage.hideResult();
             }
